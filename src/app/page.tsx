01df55d2068474243264175aebb2c7b5cb68a65b
yassine <yassine.yas98@gmail.com>
2024-05-18 22:05:12 +0100

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import { db } from "~/server/db";
+import { posts } from "~/server/db/schema";
 const mockImages = [
   "https://utfs.io/f/a2022d48-671c-4767-bb48-32842cc8bef7-4bo6yw.jpg",
   "https://utfs.io/f/9cee3877-9ba1-4a8a-9baf-fd2dbb3e416f-3vryfv.jpg",
@@ -6,7 +8,10 @@ const mockImages = [
   "https://utfs.io/f/d4945b11-5871-4d4b-a9f9-2aba80643350-66scre.jpg",
 ];
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <section className="">
       <div className="flex flex-wrap items-center justify-center gap-4">
