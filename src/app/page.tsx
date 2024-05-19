a7078346deebcfc08155e8f5f6010ff199168413
yassine <yassine.yas98@gmail.com>
2024-05-19 16:15:23 +0100

    fixing images users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 export const dynamic = "force-dynamic";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { db } from "~/server/db";
 
 export default async function HomePage() {
@@ -18,7 +19,10 @@ export default async function HomePage() {
 }
 
 async function Images() {
-  const images = await db.query.images.findMany();
+  const { userId } = auth();
+  const images = await db.query.images.findMany({
+    where: (i, { eq }) => eq(i.userId, userId!),
+  });
   return (
     <div className="flex flex-wrap items-center justify-center gap-4">
       {images.map((i) => (
